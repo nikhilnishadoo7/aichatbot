2d09c692080c25ecd0e074b05f6680a5ae76c9d7
nikhilnishadoo7 <rrnrrn007@gmail.com>
2024-06-17 23:35:12 +0530

    second commit

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './Sidebar.css'
+import {asset, assets} from '../../assets/assets'
 
 const Sidebar = () => {
   return (
-    <div className='Sidebar'>Sidebar</div>
+    <div className='Sidebar'>
+      <div className="top">
+          <img src={assets.menu_icon} alt='icon' />
+      </div>
+      <div className="bottom">
+
+      </div>
+    </div>
   )
 }
 
