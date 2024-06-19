46293ccde6fe66766f559bba4e76a08a5b736399
nikhilnishadoo7 <rrnrrn007@gmail.com>
2024-06-19 15:48:53 +0530

    project completed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,6 +5,10 @@ import { Context } from '../../context/context'
 
 const Main = () => {
     const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context)
+
+    const handleCardClick = (promptText) => {
+        setInput(promptText);
+    };
   return (
     <div className='main'>
         <div className="nav">
@@ -12,38 +16,87 @@ const Main = () => {
             <img src={assets.user_icon}/>
         </div>
         <div className="main-container">
+            {!showResult
+            ?
+            <>
             <div className="greet">
-                <p><span>Hello,Dev</span></p>
-                <p>How can i Help you today?</p>
+                <p>
+                    <span>Hello , Dev </span>
+                </p>
+                <p>How Can i Help You Today?</p>
             </div>
             <div className="cards">
-                <div className="card">
-                    <p>Sugest beautifull place to see in upcoming road trip</p>
+                <div
+                    className="card"
+                    onClick={() =>
+                        handleCardClick("Suggest Some Place To Visit In Kerala")
+                    }
+                >
+                    <p>Suggest Some Place To Visit In Kerala </p>
                     <img src={assets.compass_icon} alt="" />
                 </div>
-
-                <div className="card">
-                    <p>Briefly summarize this concept:urban planning</p>
-                    <img src={assets.bulb_icon} alt="" />
-                </div>
-
-                <div className="card">
-                    <p>Brainstorm team bonding activities for our work retreat</p>
+                <div
+                    className="card"
+                    onClick={() =>
+                        handleCardClick(
+                            "Brainstorm team bonding activities for our work retreat"
+                        )
+                    }
+                >
+                    <p>Brainstorm team bonding activities for our work retreat </p>
                     <img src={assets.message_icon} alt="" />
                 </div>
-
-                <div className="card">
-                    <p>Improve the readability of the following code</p>
+                <div
+                    className="card"
+                    onClick={() =>
+                        handleCardClick("How to Create a Gyroscope using Disc?")
+                    }
+                >
+                    <p>How to Create a Gyroscope using Disc?</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+                <div
+                    className="card"
+                    onClick={() => {
+                        handleCardClick(
+                            "Create a Script for the youtube video about coding "
+                        );
+                    }}
+                >
+                    <p>Create a Script for the youtube video about coding </p>
                     <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+        </>
+            :(
+                <div className="result">
+                    <div className="result-title">
+                        <img src={assets.user_icon} alt="" />
+                        <p>{recentPrompt}</p>
+                    </div>
+                    <div className="result-data">
+                        <img src={assets.gemini_icon} alt="" />
+                        {loading ? (
+                            <div className="loader">
+                                <hr />
+                                <hr />
+                                <hr />
+                            </div>
+                        ) : (
+                            <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                        )}
+                    </div>
+                </div>
+            )}
+
+            
             <div className="main-bottom">
                 <div className="search-box">
                     <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here' />
                     <div>
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
-                        <img onClick={(e)=>onSent()} src={assets.send_icon} alt="" />
+                        {input?<img onClick={(e)=>onSent()} src={assets.send_icon} alt="" />:null}
                     </div>
                 </div>
                 <p className='bottom-info'>
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -3,6 +3,8 @@ import run from "../config/gemini";
 
 export const Context =createContext();
 
+const ContextProvider = (props) =>{
+
 const [input,setInput] =useState("");
 const [recentPrompt,setRecentPrompt]= useState("");
 const [prevPrompts,setPrevPrompts]= useState([]);
@@ -10,14 +12,48 @@ const[showResult,setShowResult] = useState(false);
 const[loading,setLoading] = useState(false);
 const[resultData,setResultData] = useState("");
 
-const ContextProvider = (props) =>{
+const delayPara =(index,nextWord) => {
+setTimeout(function() {
+    setResultData(prev=>prev+nextWord)
+}, 75*index);
+}
+
+const newChat = () =>{
+    setLoading(false);
+    setShowResult(false)
+}
+
+const onSent = async(prompt)=>{
 
     setResultData ("");
 setLoading(true);
 setShowResult(true);
-    const onSent = async(prompt)=>{
-      const response = await run(input);
-      setResultData(response);
+let response;
+if (prompt !== undefined) {
+    response = await run(prompt);
+    setRecentPrompt(prompt)
+}
+else{
+  setPrevPrompts(prev=>[...prev,input])
+  setRecentPrompt(input)
+  response =await run(input)
+}
+      let responseArray =  response.split("**")
+      let newResponse = "" ;
+      for(let i=0;i<responseArray.length;i++){
+        if(i ==0 || i%2 !==1){
+            newResponse += responseArray[i];
+        }
+        else{
+            newResponse += "<b>" +responseArray[i]+"</b>"
+        }
+      }
+      let newResponse2 =newResponse.split("*").join("</br>")
+      let newResponseArray = newResponse2.split(" ");
+      for(let i=0;i<newResponseArray.length;i++){
+        const nextWord = newResponseArray[i];
+        delayPara(i,nextWord+" ")
+      }
       setLoading(false);
       setInput("");
     }
@@ -34,9 +70,11 @@ setShowResult(true);
      loading,
      resultData,
      input,
-     setInput
+     setInput,
+     newChat,
 
     }
+  
     return(
         <Context.Provider value={contextValue}>
         {props.children}
